3fd26f86da7a4a0296c17fc781b45f82ee451a60
Rui Silva Torres <rtorres@becker.com>
2017-06-29 17:45:17 +0100

    Adjusting the VAT calculation, considering that the amount can hold the currency code.

--- src/js/apps/codingtest/expenses/expenses-controller.js
+++ src/js/apps/codingtest/expenses/expenses-controller.js
@@ -11,6 +11,7 @@ var app = angular.module("expenses.controller", []);
 app.controller("ctrlExpenses", ["$rootScope", "$scope", "config", "restalchemy", function ExpensesCtrl($rootScope, $scope, $config, $restalchemy) {
 	
 	const VAT = 0.20;
+	const AMOUNT_REGEXP = "^(\\d*\\.?\\d*)(\\s*)([A-Z]{3})?$"
 
 	// Update the headings
 	$rootScope.mainTitle = "Expenses";
@@ -49,7 +50,18 @@ app.controller("ctrlExpenses", ["$rootScope", "$scope", "config", "restalchemy",
 	};
 
 	$scope.calculateVat = function(){
-		$scope.vat = Math.round(($scope.newExpense.amount * VAT) * 100) / 100;
+
+		//Extracting the amount and igonoring the currency
+		var regexp = new RegExp(AMOUNT_REGEXP);
+		var match = regexp.exec($scope.newExpense.amount);
+
+		if(match != null && (Number(match[1])>0)){
+			let amount = Number(match[1]);
+			$scope.vat = Math.round((amount * VAT) * 100) / 100;
+		}
+		else{
+			$scope.vat = null;
+		}
 	}
 
 	// Initialise scope variables
